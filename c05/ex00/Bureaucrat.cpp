04e95ea109f5ce7a5092cd5f7af31f28892f7d99
Hugothms <hugothms@gmail.com>
2020-12-05 15:44:25 +0100

    start c05/e00

---
+++ c05/ex00/Bureaucrat.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/05 12:13:44 by hthomas           #+#    #+#             */
+/*   Updated: 2020/12/05 15:41:18 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Bureaucrat
+{
+private:
+	/* data */
+public:
+	Bureaucrat(/* args */);
+	~Bureaucrat();
+};
+
+Bureaucrat::Bureaucrat(/* args */)
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
