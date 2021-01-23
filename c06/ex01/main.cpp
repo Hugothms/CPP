eb8a2b7444d592ee17a0912870410a0e95b03604
hugothms <hugothms@gmail.com>
2021-01-23 14:14:37 +0100

    structure for CO6/ex01-02

---
+++ c06/ex01/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/23 12:45:42 by hthomas           #+#    #+#             */
+/*   Updated: 2021/01/23 14:05:49 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream> //cout
+
+struct Data
+{
+	std::string s1;
+	int n;
+	std::string s2;
+};
+
+void*	serialize(void)
+{
+	
+}
+
+Data*	deserialize(void * raw)
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
---
+++ c06/ex02/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/23 12:45:42 by hthomas           #+#    #+#             */
+/*   Updated: 2021/01/23 14:14:15 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream> //cout
+#include"A.hpp"
+#include"B.hpp"
+#include"C.hpp"
+
+
+Base*	generate(void)
+{
+	
+}
+
+void	identify_from_pointer(Base* p)
+{
+
+}
+
+void	identify_from_reference(Base& p)
+{
+
+}
+
+int		main(int argc, char const *argv[])
+{
+	return 0;
+}
