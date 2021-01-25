84efc88b49430d3b1b8e8c7e5cf0be0601c54184
hugothms <hugothms@gmail.com>
2021-01-25 13:07:32 +0100

    in progress

--- c06/ex01/main.cpp
+++ c06/ex01/main.cpp
@@ -6,30 +6,56 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 12:45:42 by hthomas           #+#    #+#             */
-/*   Updated: 2021/01/23 14:05:49 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/01/25 09:21:49 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<iostream> //cout
-
+#include <iostream> //cout
+#include <string>
+#include <ctime> //srand rand
 struct Data
 {
-	std::string s1;
 	int n;
+	std::string s1;
 	std::string s2;
 };
 
 void*	serialize(void)
 {
-	
+	Data* ser = new Data;
+	static char	set[] = "abcdefghijklmnopqrstuvwxyz"
+                       	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                        "1234567890";
+	for (size_t i = 0; i < 8; i++)
+	{
+		std::cout << set[rand() % sizeof(set)] << std::endl;
+		ser->s1[i] = set[rand() % sizeof(set)];
+		ser->s2[i] = set[rand() % sizeof(set)];
+		std::cout << set.s1 << std::endl;
+	}
+	ser->n = rand();
+	return ser;
 }
 
-Data*	deserialize(void * raw)
+Data*	deserialize(void* raw)
 {
-
+	Data* data;
+	data = reinterpret_cast<Data*>(raw);
+	return data;
 }
 
 int main(int argc, char const *argv[])
 {
+	void* ser;
+	Data* data;
+
+    srand(time(0));
+	ser = serialize();
+	data = deserialize(ser);
+
+	std::cout 	<< data->s1 << std::endl 
+				<< data->n << std::endl
+				<< data->s2 << std::endl;
+	std::cout << data << std::endl;
 	return 0;
 }
