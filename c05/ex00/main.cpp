67ad5e62181628cfbf454d6ed406293340fcad9a
Hugothms <hugothms@gmail.com>
2021-04-14 17:37:49 +0200

    clean

--- c05/ex00/main.cpp
+++ c05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 12:13:49 by hthomas           #+#    #+#             */
-/*   Updated: 2021/04/10 19:28:30 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/04/14 17:24:24 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,20 @@ int main()
 {
 	Bureaucrat hugo = Bureaucrat("hugo", 42);
 	std::cout << hugo << std::endl;
+
 	Bureaucrat boss = Bureaucrat("boss", 3);
 	std::cout << boss << std::endl;
+
 	boss.incrementGrade();
 	std::cout << boss << std::endl;
+
 	boss.incrementGrade();
 	std::cout << boss << std::endl;
+
 	boss.incrementGrade();
 	std::cout << boss << std::endl;
+
+	Bureaucrat test = Bureaucrat("test", 1234);
+	Bureaucrat test2 = Bureaucrat("test2", 0);
 	return 0;
 }
