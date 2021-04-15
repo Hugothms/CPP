46db9f867cdfe70fb71051f05e00a51c013757d7
Hugothms <hugothms@gmail.com>
2021-04-15 17:29:20 +0200

    c07ok

--- c07/ex02/main.cpp
+++ c07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 14:56:10 by hthomas           #+#    #+#             */
-/*   Updated: 2021/04/15 15:42:10 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/04/15 16:07:53 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,15 @@ void	print(Array<T> &array)
 {
 	std::cout << "---" << std::endl;
 	for (size_t i = 0; i < array.size(); i++)
-	{
-		array[i] = i;
 		std::cout << array[i] << " ";
-	}
 	std::cout << std::endl;
+}
+
+template<typename T>
+void	fill(Array<T> &array)
+{
+	for (size_t i = 0; i < array.size(); i++)
+		array[i] = i * 1.1;
 }
 
 int main(void)
@@ -36,19 +40,21 @@ int main(void)
 	Array<int> intRR(5);
 	print(intRR);
 
+	fill(intRR);
 	intR = intRR;
 	print(intR);
 
 
-
 	std::cout << std::endl << "FLOAT" << std::endl;
 	Array<float> floatR(25);
+	fill(floatR);
 	print(floatR);
-
 
 
 	std::cout << std::endl << "STRING" << std::endl;
 	Array<std::string> stringR(5);
+	stringR[0] = "Hello World!";
+	stringR[1] = "AZERTY!";
 	print(stringR);
 
 	Array<std::string> stringRR = stringR;
@@ -56,7 +62,6 @@ int main(void)
 		stringRR[i] = "42";
 	print(stringRR);
 	print(stringR);
-
 
 
 	try
