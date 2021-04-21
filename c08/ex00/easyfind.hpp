abe2c6bc4f0cc8031b541cdd37ead8df413cd041
Hugothms <hugothms@gmail.com>
2021-04-21 13:51:27 +0200

    c08/ex00 OK

--- c08/ex00/easyfind.hpp
+++ c08/ex00/easyfind.hpp
@@ -6,13 +6,31 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 10:07:59 by hthomas           #+#    #+#             */
-/*   Updated: 2021/04/21 11:38:14 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/04/21 13:39:19 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template <typename T>
-typename T::iterator	easyfind(const T& container, int value)
-{
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
 
-	return 0;
+#include <algorithm>
+#include <exception>
+
+class NotFoundException: public std::exception
+{
+	virtual const char* what() const throw()
+	{
+		return ("NotFoundException: Item not found");
+	}
+};
+
+template <typename T>
+typename T::iterator	easyfind(T &container, int value)
+{
+	typename T::iterator iterator = std::find(container.begin(), container.end(), value);
+	if (iterator == container.end())
+		throw NotFoundException();
+	return (iterator);
 }
+
+#endif
--- c08/ex00/main.cpp
+++ c08/ex00/main.cpp
@@ -6,12 +6,67 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 10:07:57 by hthomas           #+#    #+#             */
-/*   Updated: 2021/04/21 11:38:23 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/04/21 13:51:17 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"easyfind.hpp"
+#include<vector>
+#include<list>
+#include<iostream>
+
 int main()
 {
-	/* code */
+	/**** VALUES TO SEARCH FOR ****/
+	std::vector<int> values;
+	values.push_back(42);
+	values.push_back(43);
+
+
+	/**** SEARCH IN VECTOR ****/
+	std::vector<int> vect;
+	vect.push_back(1);
+	vect.push_back(-999);
+	vect.push_back(42);
+	vect.push_back(1996);
+
+	for (size_t i = 0; i < values.size(); i++)
+	{
+		int value = values[i];
+		std::cout << "Searched " << value << ": ";
+		try
+		{
+			std::vector<int>::iterator found = easyfind(vect, value);
+			std::cout << *found << " found !" << std::endl;
+		}
+		catch(std::exception &e)
+		{
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
+
+
+
+	/**** SEARCH IN LIST ****/
+	std::list<int> list;
+	list.push_back(1);
+	list.push_back(-999);
+	list.push_back(42);
+	list.push_back(1996);
+
+	for (size_t i = 0; i < values.size(); i++)
+	{
+		int value = values[i];
+		std::cout << "Searched " << value << ": ";
+		try
+		{
+			std::list<int>::iterator found = easyfind(list, value);
+			std::cout << *found << " found !" << std::endl;
+		}
+		catch(std::exception &e)
+		{
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
 	return 0;
 }
