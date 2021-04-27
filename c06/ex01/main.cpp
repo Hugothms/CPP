14faf4627e6367bee66224be74ba0b926ab85c75
Hugothms <hugothms@gmail.com>
2021-04-27 17:12:32 +0200

    clean

--- c06/ex01/main.cpp
+++ c06/ex01/main.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************** */
-/*                                                                            */
+/*	                                                                          */
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
@@ -17,45 +17,46 @@
 
 struct Data
 {
-    int            n;
-    std::string    s1;
-    std::string    s2;
+	int			n;
+	std::string	s1;
+	std::string	s2;
 };
 
 void*	serialize(void)
 {
-    Data           *ret = new Data;
-    static char    set[] = "abcdefghijklmnopqrstuvwxyz"
-                           "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                           "1234567890";
+	Data			*ret = new Data;
+	static char	set[] = "abcdefghijklmnopqrstuvwxyz"
+							"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+							"1234567890";
 
-    for (int i = 0; i < 8; i += 1)
-    {
-        ret->s1 += set[std::rand() % sizeof(set)];
-    }
-    ret->n = std::rand() * (std::rand() & 1 ? 1 : -1);
-    for (int i = 0; i < 8; i += 1)
-    {
-        ret->s2 += set[std::rand() % sizeof(set)];
-    }
-    return (ret);
+	for (int i = 0; i < 8; i += 1)
+	{
+		ret->s1 += set[std::rand() % sizeof(set)];
+	}
+	ret->n = std::rand() * (std::rand() & 1 ? 1 : -1);
+	for (int i = 0; i < 8; i += 1)
+	{
+		ret->s2 += set[std::rand() % sizeof(set)];
+	}
+	return (ret);
 }
 
 Data*	deserialize(void *raw)
 {
-    Data           *ret = reinterpret_cast<Data*>(raw);
-    return (ret);
+	Data			*ret = reinterpret_cast<Data*>(raw);
+	return (ret);
 }
 
 int		main(void)
 {
-    srand(time(0));
-    void           *ser = serialize();
-    Data           *des = deserialize(ser);
+	srand(time(0));
+	void			*ser = serialize();
+	Data			*des = deserialize(ser);
 
-    std::cout << des->s1 << std::endl \
-              << des->n << std::endl \
-              << des->s2 << std::endl;
-    delete des;
-    return (0);
+	std::cout << des->s1 << std::endl;
+	std::cout << des->n << std::endl;
+	std::cout << des->s2 << std::endl;
+
+	delete des;
+	return (0);
 }
