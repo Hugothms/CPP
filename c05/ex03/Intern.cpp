6fcc3fdcfbba769ce03ac4c1edb12e0b3db50950
Hugothms <hugothms@gmail.com>
2021-07-07 16:00:47 +0200

    remove useless typedef on the array of pointers to functions

--- c05/ex03/Intern.cpp
+++ c05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 12:13:44 by hthomas           #+#    #+#             */
-/*   Updated: 2021/06/08 16:42:56 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/07/07 15:57:33 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,31 @@ Intern::Intern()
 Intern::~Intern()
 {}
 
-Form*	new_shrub(const std::string& target) {
+Form*	new_shrub(const std::string& target)
+{
 	return new ShrubberyCreationForm(target);
 }
 
-Form*	new_pardon(const std::string& target) {
+Form*	new_pardon(const std::string& target)
+{
 	return new PresidentialPardonForm(target);
 }
 
-Form*	new_robo(const std::string& target) {
+Form*	new_robo(const std::string& target)
+{
 	return new RobotomyRequestForm(target);
 }
-
-typedef Form *(*t_make_funct)(const std::string&);
 
 Form*	Intern::makeForm(const std::string& name, const std::string& formtarget) const {
 	const std::string types[3] = {	"PresidentialPardon",
 									"RobotomyRequest",
 									"ShrubberyCreation"};
-	static t_make_funct makeFormFuncts[3] = {new_pardon, new_robo, new_shrub};
+	static Form* (*makeFormFuncts[3])(const std::string&) = {new_pardon, new_robo, new_shrub};
 
-	for (int i = 0; i < 3; i++) {
-		if (name == types[i]) {
+	for (int i = 0; i < 3; i++)
+	{
+		if (name == types[i])
+		{
 			std::cout << "Intern creates " << name << "." << std::endl;
 			return makeFormFuncts[i](formtarget);
 		}
